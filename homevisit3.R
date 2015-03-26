f259edff8767ffb57e5f077dfa66093b7d71086a
Edouard <edouard.legoupil@gmail.com>
2015-03-26 12:28:13 +0200

    rev

--- homevisit3.R
+++ homevisit3.R
@@ -31,17 +31,21 @@ homevisit3 <-homevisit3[!rowSums(is.na(homevisit3["long"])), ]
 
 # str(homevisit3)
 
+
+###############################################################
 ## Create a spatial data frame with vaf records
 rm(datasp)
 datasp1 <-homevisit3[ , c("long", "lat")]
 # summary(datasp)
 coords <- cbind(datasp1$long, datasp1$lat)
 datasp <- SpatialPointsDataFrame(coords, data= datasp1, proj4string=CRS("+proj=longlat"))
-
 plot(datasp)
 
-### Map Overlay in gplot2
 
+
+###############################################################
+### Simple Map Overlay in gplot2
+##############################################################
 require("ggmap")
 
 
@@ -59,6 +63,10 @@ map1 <- map1 +
 ggsave("out/map1.png", map1, width=8, height=6,units="in", dpi=300)
 
 
+###############################################################
+### Hebin Map Overlay in gplot2
+##############################################################
+
 homevisit4 <-homevisit3[!rowSums(is.na(homevisit3["Household.information.Family.Size"])), ]
 require("hexbin")
 rm(map2)
@@ -73,3 +81,62 @@ map2 <- map2+
 
 # Save this!
 ggsave("out/map2.png", map2, width=8, height=6,units="in", dpi=300)
+
+###############################################################
+### Graph example with gplot2
+##############################################################
+
+# Reorgnise the matrix in order to compute pivot tables
+# Tuto: http://marcoghislanzoni.com/blog/2013/10/11/pivot-tables-in-r-with-melt-and-cast/
+
+require("reshape2")
+####### Let's graph Assets
+
+# extract on asset per family size
+homevisit3.assets <- melt(homevisit3, id=c("Household.information.Family.Size"),
+                          measure=c("Type.of.Housing.Assets...Floor.mattress",
+                                                           "Type.of.Housing.Assets...Sofa.set",
+                                                           "Type.of.Housing.Assets...Kitchen.utilities",
+                                                           "Type.of.Housing.Assets...Computer",
+                                                           "Type.of.Housing.Assets...Blankets",
+                                                           "Type.of.Housing.Assets...Stove",
+                                                           "Type.of.Housing.Assets...Washing.machine",
+                                                           "Type.of.Housing.Assets...Table-chairs",
+                                                           "Type.of.Housing.Assets...Cabinets",
+                                                           "Type.of.Housing.Assets...Fridge",
+                                                           "Type.of.Housing.Assets...Television",
+                                                           "Type.of.Housing.Assets...Water.heater",
+                                                           "Type.of.Housing.Assets...Freezer",
+                                                           "Type.of.Housing.Assets...Other..specify"))
+
+
+## Reorder factor level according to a value
+
+
+homevisit3.assets <- homevisit3.assets[order(-homevisit3.assets$value), ]
+
+#homevisit3.assets$variable <- as.character(homevisit3.assets$variable)
+
+#str(homevisit3.assets)
+
+#homevisit3.assets$variable <- factor(homevisit3.assets$variable , levels = homevisit3.assets[order(homevisit3.assets$value), 1])
+
+levels(homevisit3.assets$variable)
+
+#
+homevisit3.assets2 <- dcast(homevisit3.assets, Household.information.Family.Size ~ variable)
+
+
+
+# Bar graph based on reordered variable per value
+asset.plot <- ggplot(data=homevisit3.assets, aes(x=reorder(variable, value) , y=value, fill=variable)) + 
+  geom_bar( stat="identity",fill="#2a87c8",colour="#2a87c8") +
+ # geom_text(aes(label=variable), vjust=0) +
+  guides(fill=FALSE) + 
+  coord_flip()+
+  xlab("Asset") + 
+  ylab("Household") +
+  ggtitle("Household assets")
+
+# Save this!
+ggsave("out/assetplot.png", asset.plot, width=8, height=6,units="in", dpi=300)
